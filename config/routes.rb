ab5fc1d10d5410bac6cfc050d7e55f6496ce7eb4
LuciaMarcu <lucia_sanzianu@yahoo.com>
2021-10-12 18:10:07 +0300

    Add user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  
+  get 'users/show'
   resources :prints do
     resources :comments
     member do 
@@ -8,6 +9,8 @@ Rails.application.routes.draw do
   	end
   end
   
-  devise_for :users, controllers: { registrations: 'registrations'}
+  match '/users/:id', to: 'users#show', via: 'get' 
+  devise_for :users, controllers: { registrations: 'registrations'}, :path_prefix => 'd'
+  resources :users, :only =>[:show]
   root 'prints#index'
 end
